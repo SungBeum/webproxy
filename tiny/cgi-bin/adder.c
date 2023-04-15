5efb736d98317f8009e826cfe3f988482e8f3ac5
Ubuntu <ubuntu@ip-172-31-34-117.ap-northeast-2.compute.internal>
2023-04-15 16:18:13 +0000

    04.16

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -1,10 +1,39 @@
 /*
- * adder.c - a minimal CGI program that adds two numbers together
+ * adder.c - 두 개의 정수를 더하는 CGI 프로그램
  */
-/* $begin adder */
 #include "csapp.h"
 
 int main(void) {
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1=0, n2=0;
+  char *method = getenv("REQUEST_METHOD");
+
+  if ((buf = getenv("QUERY_STRING")) != NULL) {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p+1);
+    p = strchr(arg1, '=');
+    n1 = atoi(p+1);
+    p = strchr(arg2, '=');
+    n2 = atoi(p+1);
+  }
+
+
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  if (strcasecmp(method, "GET") == 0)
+  {
+    printf("%s", content);
+  }
+  fflush(stdout);
   exit(0);
 }
-/* $end adder */
